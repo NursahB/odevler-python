4cccc1650cff0b9496eaea5addc7656cfcdb0f07
NursahB <51656380+NursahB@users.noreply.github.com>
2019-06-20 15:03:34 -0700

    Add files via upload

---
+++ 1.hafta 6.soru.py
@@ -0,0 +1,9 @@
+print("""Python'un kurucusu Guido Van Rossum""","""Hollanda'nin Amsterdam'daki""","""\"Universiteit Van Amsterdam"okulundan""","""1982'de mezun olmustur.""",sep='\n')
+
+print("""
+Python'un kurucusu Guido Van Rossum
+Hollanda'nin Amsterdam'daki
+"Universiteit Van Amsterdam"okulundan
+1982'de mezun olmustur.""")
+
+print('Python\'un kurucusu Guido Van Rossum','Hollanda\'nin Amsterdam\'daki','"Universiteit Van Amsterdam"okulundan','1982\'de mezun olmustur.',sep='\n')
